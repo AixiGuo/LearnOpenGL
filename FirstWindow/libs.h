aff158e7ba5263904274fa5db23586c00ff9ddaf
AixiGuo <aixi.guo1990@gmail.com>
2020-05-22 14:31:54 +0200

    Init

---
+++ FirstWindow/libs.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <glad.h>
+#include <glfw3.h>
+
+#include <glm.hpp>
+#include <vec2.hpp>
+#include <vec3.hpp>
+#include <vec4.hpp>
+#include <mat4x4.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+#include <SOIL2.h>
+ 
